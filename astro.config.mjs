d48e95f90cbbe22be236033861f71cd1b02ec135
kkAyataka <kk.ayataka@gmail.com>
2026-01-25 11:06:29 +0900

    docs: update the title and the link to GitHub

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Study Astro',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/kkayataka/study-astro' }],
 			sidebar: [
 				{
 					label: 'Guides',
