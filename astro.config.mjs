550532a7664a3c715ef0d1f8764e7ed71ac6b680
kkAyataka <kk.ayataka@gmail.com>
2026-01-31 21:59:40 +0900

    feat: set site and base settings for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://github.com/kkayataka/',
+	base: 'study-astro',
 	integrations: [
 		starlight({
 			title: 'Study Astro',
